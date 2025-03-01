bfcc176b38440e3e491f71b5f454417e7cd5a1ce
grabercn <cgrab713@gmail.com>
2025-03-01 13:34:06 -0500

    reorganized front end components, created component files, added public items, fixed deployment rendering

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      {/* TODO: Render top-level components here */}
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default App;
