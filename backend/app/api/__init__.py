3982ea0c1e4a2eafb25bc4d929303aeb6c8636e1
Owen Edwards <edwardoa@mail.uc.edu>
2025-03-02 06:51:12 -0500

    Cleaned up code comments

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -2,10 +2,6 @@ from flask import Blueprint
 
 # Define Blueprints for each route
 resume_upload_blueprint = Blueprint('resume_upload', __name__, url_prefix='/api/upload')
-# resume_analysis_blueprint = Blueprint('resume_analysis', __name__, url_prefix='/api/analysis')
-# resume_comparison_blueprint = Blueprint('resume_comparison', __name__, url_prefix='/api/comparison')
 
 # Import routes
 from .resume_endpoints import *
-# from .resume_analysis import *
-# from .resume_comparison import *
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
 from app.api.resume_endpoints import resume_upload_blueprint
-# from app.api.resume_analysis import resume_analysis_blueprint
-# from app.api.resume_comparison import resume_comparison_blueprint
 
 def create_app():
     app = Flask(__name__)
@@ -10,10 +8,8 @@ def create_app():
     # Enable CORS for all routes
     CORS(app, resources={r"/api/*": {"origins": ["https://main.dgiaauliixglz.amplifyapp.com", "http://localhost:5173"]}})  # Allow only requests from this origin
 
-    # Register Blueprints for different API endpoints
+    # Register Blueprints for the API endpoints
     app.register_blueprint(resume_upload_blueprint, url_prefix='/api')
-    # app.register_blueprint(resume_analysis_blueprint)
-    # app.register_blueprint(resume_comparison_blueprint)
 
     return app
 
